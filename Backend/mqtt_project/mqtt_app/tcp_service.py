f90f8936f3ccbb4b088030f123d45d28af7af3c9
mohammad <mohammad.estaki@tapsi.cab>
2025-01-27 21:27:32 +0330

    fix ecg input type

--- Backend/mqtt_project/mqtt_app/tcp_service.py
+++ Backend/mqtt_project/mqtt_app/tcp_service.py
@@ -1,13 +1,22 @@
+import json
 import socket
 import threading
+from datetime import datetime
 
 def handle_client(client_socket):
     """Handles client communication."""
     with client_socket:
         print("Connection established.")
-        data = client_socket.recv(1024)
+        data = client_socket.recv(1048576)
         if data:
+            print(data)
             print(f"Received: {data.decode()}")
+            print(json.load(data.decode()))
+            if json.load(data.decode()).has_key("has_fallen"):
+                handel_acl_message()
+
+            else:
+                handle_ecg_message(json.loads(data.decode())["ecg_record"])
 
 def start_tcp_server(host='0.0.0.0', port=1234):
     """Starts the TCP server."""
@@ -22,6 +31,24 @@ def start_tcp_server(host='0.0.0.0', port=1234):
             # Start a new thread to handle the client
             client_thread = threading.Thread(target=handle_client, args=(client_socket,))
             client_thread.start()
+
+def handel_acl_message():
+    from .models import Fallen
+    fallen = Fallen(last_fallen_time=datetime.now())
+    fallen.save()
+
+def handle_ecg_message(payload):
+    from .models import Record
+    # print(payload.decode())
+    # print(json.loads(payload.decode()))
+    # rec = json.loads(payload.decode())["value"]
+    print(payload)
+    # print(int(float(payload.decode())))
+    for item in payload:
+        timestamp = int(item["timestamp"])
+        value = item["value"]
+        record = Record(integer_field=value, time_field=datetime.fromtimestamp(timestamp))
+        record.save()
 
 if __name__ == "__main__":
     start_tcp_server()
