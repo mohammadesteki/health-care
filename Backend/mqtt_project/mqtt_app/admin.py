1c905cc7a5772d9fb216c3004dd7c6d21714d327
mohammad <mohammad.estaki@tapsi.cab>
2025-01-30 01:16:06 +0330

    Add gps to admin

--- Backend/mqtt_project/mqtt_app/admin.py
+++ Backend/mqtt_project/mqtt_app/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Record
 from .models import Fallen
+from .models import GPS
 # Register your models here.
 admin.site.register(Record)
 admin.site.register(Fallen)
+admin.site.register(GPS)
